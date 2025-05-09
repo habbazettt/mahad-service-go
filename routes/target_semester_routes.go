179f3158b35cba1319a056c8ae66b2f0a2259414
Hubbal Kholiq Habbaza <habbazahubbal@gmail.com>
2025-05-09 15:56:07 +0700

    feat(target): add mahasantri role access for get target semester data

--- routes/target_semester_routes.go
+++ routes/target_semester_routes.go
@@ -39,8 +39,8 @@ func SetupTargetSemesterRoutes(app *fiber.App, db *gorm.DB) {
 	targetSemesterRoutes := app.Group("/api/v1/target_semester", middleware.JWTMiddleware, methodLimiter)
 	{
 		targetSemesterRoutes.Post("/", middleware.RoleMiddleware("mentor"), service.CreateTargetSemester)
-		targetSemesterRoutes.Get("/", middleware.RoleMiddleware("mentor"), service.GetAllTargetSemesters)
-		targetSemesterRoutes.Get("/:id", middleware.RoleMiddleware("mentor"), service.GetTargetSemesterByID)
+		targetSemesterRoutes.Get("/", middleware.RoleMiddleware("mentor", "mahasantri"), service.GetAllTargetSemesters)
+		targetSemesterRoutes.Get("/:id", middleware.RoleMiddleware("mentor", "mahasantri"), service.GetTargetSemesterByID)
 		targetSemesterRoutes.Get("/mahasantri/:mahasantri_id", middleware.RoleMiddleware("mentor", "mahasantri"), service.GetTargetSemesterByMahasantriID)
 		targetSemesterRoutes.Put("/:id", middleware.RoleMiddleware("mentor"), service.UpdateTargetSemester)
 		targetSemesterRoutes.Delete("/:id", middleware.RoleMiddleware("mentor"), service.DeleteTargetSemester)
