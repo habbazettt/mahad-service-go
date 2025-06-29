688687c81bafc309969f0ef6b7bc5b60c18ba52b
Hubbal Kholiq Habbaza <habbazahubbal@gmail.com>
2025-06-29 17:18:49 +0700

    feat(cors): add frontend url for CORS

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "https://mtadigital.netlify.app",
 		AllowMethods: "GET,POST,PUT,DELETE",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
